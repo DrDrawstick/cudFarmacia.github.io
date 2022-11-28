23e1d0c67b62b8c204c6006f09b479e17c961880
DrDrawstick <felipe.rojaspincheira@gmail.com>
2022-11-28 18:21:31 -0300

    first commit

---
+++ database/migrations/2022_11_28_203216_create_medicamentos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMedicamentosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medicamentos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string("nombre");
+            $table->string("laboratorio");
+            $table->integer("lote");
+            $table->date("fecha_elavoracion");
+            $table->string("fecha_expiracion");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medicamentos');
+    }
+}
